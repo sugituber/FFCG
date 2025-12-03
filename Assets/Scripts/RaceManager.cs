dccdc285a96623dfe8dbcd5b52d66dae6a5ef0aa
Sara <saraelisabet@gmail.com>
2025-12-03 13:56:32 +0000

    Create RaceManager.cs

---
+++ Assets/Scripts/RaceManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class RaceStart : MonoBehaviour
+{
+    public TextMeshProUGUI countdownText;
+    public TextMeshProUGUI timerText;
+    private float raceTime = 0f;
+    private bool raceStarted = false;
+    public CarController car;
+
+    void Start()
+    {
+        Debug.Log("RaceStart Start() is running!");
+        car.enabled = false;
+        StartCoroutine(StartCountdown());
+    }
+
+    IEnumerator StartCountdown()
+    {
+        int countdown = 3;
+
+        while (countdown > 0)
+        {
+            countdownText.text = countdown.ToString();
+            yield return new WaitForSeconds(1f);
+            countdown--;
+        }
+
+        countdownText.text = "START!";
+        raceStarted = true;
+        car.enabled = true;
+
+        yield return new WaitForSeconds(1f);
+
+        countdownText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (raceStarted)
+        {
+            raceTime += Time.deltaTime;
+            timerText.text = raceTime.ToString("F2");
+        }
+    }
+}
