6d4eeb473d016c324c7f7b1208ebc026fbffe9b4
yingudna <yingudna@gmail.com>
2025-12-04 14:19:51 +0000

    ADD:: finishLine

--- Assets/Scripts/RaceManager.cs
+++ Assets/Scripts/RaceManager.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
-using TMPro;
 using System.Collections;
 
 public class RaceStart : MonoBehaviour
 {
-    public TextMeshProUGUI countdownText;
-    public TextMeshProUGUI timerText;
-    private float raceTime = 0f;
+    public TMPro.TextMeshProUGUI countdownText;
+    public TMPro.TextMeshProUGUI timerText;
+    // private float raceTime = 0f;
     private bool raceStarted = false;
     public CarController car;
+    public TimeLogic timeLogic;
 
     void Start()
     {
@@ -31,6 +31,7 @@ public class RaceStart : MonoBehaviour
         countdownText.text = "START!";
         raceStarted = true;
         car.enabled = true;
+        timeLogic.StartTimer();
 
         yield return new WaitForSeconds(1f);
 
@@ -41,8 +42,9 @@ public class RaceStart : MonoBehaviour
     {
         if (raceStarted)
         {
-            raceTime += Time.deltaTime;
-            timerText.text = raceTime.ToString("F2");
+            // raceTime += Time.deltaTime;
+            // timerText.text = raceTime.ToString("F2");
+            timerText.text = timeLogic.TimeFormat();
         }
     }
 }
